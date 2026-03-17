a6bb44475ab2a100ae92d2a2c72d960e6cd963dd
Evgeniy Sedov <sedoff-online@yandex.ru>
2026-03-18 02:01:15 +0700

    feat(ui): обновляет контент футера

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -5,68 +5,22 @@ import Logo from "@/components/Logo.vue";
 <template>
     <div class="footer border-t border-[#F3F3F3] py-8">
         <div
-            class="mx-auto grid w-fit max-w-[1220px] grid-cols-1 gap-y-8 px-4 md:grid-cols-2 md:px-6 lg:gap-x-12 lg:px-8 xl:grid-cols-4 xl:gap-x-[88px]"
-        >
+            class="mx-auto grid w-fit max-w-305 grid-cols-1 gap-y-8 px-4 md:grid-cols-2 md:px-6 lg:gap-x-12 lg:px-8 xl:grid-cols-3 xl:gap-x-22">
             <div class="flex flex-col">
                 <ul class="m-0 flex flex-col p-0">
                     <li>
                         <Logo />
                     </li>
-                    <li class="my-6 w-33.5">
-                        <div class="grid grid-cols-2 grid-rows-2 gap-1.5">
-                            <div
-                                class="flex h-8 w-16 items-center justify-center rounded-sm border-1 border-[#EAEAEA]"
-                            >
-                                <img
-                                    src="/icons/cards-visa.png"
-                                    alt="Оплата картой visa"
-                                />
-                            </div>
-                            <div
-                                class="flex h-8 w-16 items-center justify-center rounded-sm border-1 border-[#EAEAEA]"
-                            >
-                                <img
-                                    src="/icons/cards-master.png"
-                                    alt="Оплата master card"
-                                />
-                            </div>
-                            <div
-                                class="flex h-8 w-16 items-center justify-center rounded-sm border-1 border-[#EAEAEA]"
-                            >
-                                <img
-                                    src="/icons/cards-webmoney.png"
-                                    alt="Оплата через WebMoney"
-                                />
-                            </div>
-                            <div
-                                class="flex h-8 w-16 items-center justify-center rounded-sm border-1 border-[#EAEAEA]"
-                            >
-                                <img
-                                    src="/icons/cards-mir.png"
-                                    alt="Оплата картой Мир"
-                                />
-                            </div>
-                        </div>
-                    </li>
-                    <li class="mb-5">
-                        <div class="icons-social flex gap-[5px]">
-                            <img
-                                src="/icons/i-social-vk.png"
-                                alt="Иконка социальной сети вконтакте"
-                            />
-                            <img
-                                src="/icons/i-social-tg.png"
-                                alt="Иконка социальной сети телеграмм"
-                            />
-                            <img
-                                src="/icons/i-social-whatsapp.png"
-                                alt="Иконка социальной сети WhatsApp"
-                            />
+                    <li class="my-6">
+                        <div class="icons-social flex gap-1.25">
+                            <img src="/icons/i-social-vk.png" alt="Иконка социальной сети вконтакте" />
+                            <img src="/icons/i-social-tg.png" alt="Иконка социальной сети телеграмм" />
+                            <img src="/icons/i-social-whatsapp.png" alt="Иконка социальной сети WhatsApp" />
                         </div>
                     </li>
                     <li class="mt-4">
                         <span class="text-xs leading-relaxed text-[#2C2C2C]">
-                            Copyright © 2014 – 2025 Popechati.com
+                            Copyright © 2026 svai12.ru
                         </span>
                     </li>
                 </ul>
@@ -78,44 +32,23 @@ import Logo from "@/components/Logo.vue";
                             Контакты
                         </p>
                     </li>
-                    <li class="mb-[13px]">
-                        <div class="flex items-center gap-2">
-                            <a href="tel:+79254709400" class="flex">
-                                <p
-                                    class="w-fit rounded-md px-1 py-0.5 text-base/6.5 font-semibold text-[#2c2c2c] transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]"
-                                >
-                                    +7 (925) 470-94-00
-                                </p>
-                            </a>
-                            <span class="flex gap-1.5">
-                                <img
-                                    src="/icons/i-social-whatsapp.png"
-                                    alt="Иконка социальной сети WhatsApp"
-                                    class="h-4 w-4"
-                                />
-                                <img
-                                    src="/icons/i-social-tg.png"
-                                    alt="Иконка социальной сети телеграмм"
-                                    class="h-4 w-4"
-                                />
-                            </span>
-                        </div>
-                    </li>
-                    <li class="mb-4">
-                        <a
-                            class="rounded-md px-1 py-0.5 text-base/6.5 font-semibold text-[#FF8D00] transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]"
-                            href="mailto:popechati@gmail.com"
-                        >
-                            popechati@gmail.com
+                    <li class="mb-3.25">
+                        <a href="tel:+79877330011" class="flex w-fit">
+                            <p
+                                class="w-fit rounded-md px-1 py-0.5 text-base/6.5 font-semibold text-content-primary transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]">
+                                +7 (987) 733-00-11
+                            </p>
                         </a>
                     </li>
-                    <li
-                        class="text-sm leading-relaxed font-normal text-[#2C2C2C]"
-                    >
+                    <li class="mb-4">
+                        <a class="rounded-md px-1 py-0.5 text-base/6.5 font-semibold text-[#FF8D00] transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]"
+                            href="mailto:svai12@mail.ru">
+                            svai12@mail.ru
+                        </a>
+                    </li>
+                    <li class="text-sm leading-relaxed font-normal text-[#2C2C2C]">
                         <span class="block max-w-73 leading-relaxed">
-                            г. Москва, Батайский проезд 59, вход с противоположной стороны от
-                            подъездов, под вывеской «КОПИ ЦЕНТ», 7 минут пешком от метро
-                            Марьино
+                            Республика Марий Эл, Оршанский район, д.Лужбеляк, ул.Центральная,25
                         </span>
                     </li>
                 </ul>
@@ -128,60 +61,11 @@ import Logo from "@/components/Logo.vue";
                         </p>
                     </li>
                     <li class="">
-                        <ul
-                            class="text-sm leading-[25px] font-normal text-[#2C2C2C] underline"
-                        >
+                        <ul class="text-sm leading-6.25 font-normal text-[#2C2C2C] underline">
                             <li><span>Контакты</span></li>
-                            <li><span>Требования к макетам</span></li>
-                            <li class="flex flex-wrap">
-                                <span>Оплата</span>,
-                                <span class="ml-1">доставка</span>,
-                                <span class="ml-1">возврат</span>
-                            </li>
                             <li><span>Реальные отзывы</span></li>
                             <li><span>Пользовательское соглашение</span></li>
                             <li><span>Публичная оферта</span></li>
-                            <li><span>Каталог материалов</span></li>
-                            <li><span>Переделка и возврат</span></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div class="flex flex-col">
-                <ul class="m-0 flex flex-col p-0">
-                    <li class="mb-4.5">
-                        <p class="text-lg/5.5 font-medium text-[#2C2C2C]">
-                            Популярные услуги
-                        </p>
-                    </li>
-                    <li class="">
-                        <ul
-                            class="text-sm leading-[25px] font-normal text-[#2C2C2C] underline"
-                        >
-                            <li class="flex flex-wrap">
-                                <span>Печать визиток</span>,
-                                <span class="ml-1">листовок</span>
-                            </li>
-                            <li><span>Печать на баннере</span></li>
-                            <li><span>Календари</span></li>
-                            <li class="flex flex-wrap">
-                                <span>Печать плакатов</span>,
-                                <span class="ml-1">наклеек</span>
-                            </li>
-                            <li class="flex flex-wrap">
-                                <span>Roll Up</span>,
-                                <span class="ml-1">Press Wall</span>
-                            </li>
-                            <li class="flex flex-wrap">
-                                <span>Печать на кружках</span>,
-                                <span class="ml-1">ручках</span>
-                            </li>
-                            <li class="flex flex-wrap">
-                                <span>Холсты</span>,
-                                <span class="ml-1">фотоколлажи</span>,
-                                <span class="ml-1">пазлы</span>
-                            </li>
-                            <li><span>Все услуги</span></li>
                         </ul>
                     </li>
                 </ul>

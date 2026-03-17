a487207580114c0a25724c947b17776ef84abdb0
Evgeniy Sedov <sedoff-online@yandex.ru>
2026-03-17 23:06:40 +0700

    feat(ui): обновляет контакты и кнопку в хедере

--- app/components/layout/Header.vue
+++ app/components/layout/Header.vue
@@ -15,35 +15,22 @@ const openOrderModal = inject("openOrderModal") as () => void;
             <Logo class="mr-10" />
             <Menu class="mr-auto hidden xl:flex" />
 
-            <div
-                class="mr-8 hidden items-center space-x-4 text-sm md:ml-auto! md:flex"
-            >
+            <div class="mr-8 hidden items-center space-x-4 text-sm md:ml-auto! md:flex">
                 <div class="flex flex-col items-end">
                     <div class="flex items-center justify-end gap-1">
                         <span class="flex">
-                            <img
-                                src="/icons/i-social-whatsapp.png"
-                                alt="Иконка социальной сети WhatsApp"
-                                class="mr-1 h-4 w-4"
-                            />
-                            <img
-                                src="/icons/i-social-tg.png"
-                                alt="Иконка социальной сети телеграмм"
-                                class="mr-1 h-4 w-4"
-                            />
+                            <img src="/icons/i-social-whatsapp.png" alt="Иконка социальной сети WhatsApp"
+                                class="mr-1 h-4 w-4" />
+                            <img src="/icons/i-social-tg.png" alt="Иконка социальной сети телеграмм"
+                                class="mr-1 h-4 w-4" />
                         </span>
-                        <a
-                            href="tel:+79254709400"
-                            class="rounded-md px-1 py-0.5 font-semibold whitespace-nowrap text-gray-900 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]"
-                            >+7 (925) 470-94-00</a
-                        >
+                        <a href="tel:+79021091101"
+                            class="rounded-md px-1 py-0.5 font-tt-norms text-base font-medium leading-none tracking-[0.02em] uppercase text-center align-middle whitespace-nowrap text-gray-900 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-brand-light">+7
+                            (902) 109-11-01</a>
                     </div>
 
-                    <a
-                        href="mailto:POPECHATI@GMAIL.COM"
-                        class="rounded-md px-1 py-0.5 text-xs whitespace-nowrap text-gray-600 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#FFF3E4]"
-                        >popechati@gmail.com</a
-                    >
+                    <a href="mailto:svai12@mail.ru"
+                        class="rounded-md px-1 py-0.5 text-xs whitespace-nowrap text-gray-600 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-brand-light">svai12@mail.ru</a>
                 </div>
             </div>
 
@@ -52,11 +39,10 @@ const openOrderModal = inject("openOrderModal") as () => void;
             </div>
 
             <Button
-                class="hidden rounded-md border-2 border-[#B16303] bg-[#FF8D00] px-6 py-3 text-base font-semibold tracking-wider text-white shadow-lg transition-all duration-300 hover:bg-[#B16303] xl:flex"
-                @click="openOrderModal"
-            >
+                class="hidden rounded-md border-2 border-brand-border bg-brand px-6 py-3 text-base font-semibold tracking-wider text-white shadow-lg transition-all duration-300 hover:bg-brand-border xl:flex"
+                @click="openOrderModal">
                 <BasketShopping class="mr-2 h-4 w-4" />
-                Создать заказ
+                Обратный звонок
             </Button>
         </div>
     </div>
@@ -66,8 +52,9 @@ const openOrderModal = inject("openOrderModal") as () => void;
 .header {
     width: 100%;
 }
+
 .container {
-    max-width: 1590px;
+    max-width: 1580px;
     margin: 0 auto;
     padding: 12px 10px 0;
 }

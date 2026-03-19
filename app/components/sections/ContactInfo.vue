86135bc3d83c1bf67eca047b028006f11c5a74c5
Evgeniy Sedov <sedoff-online@yandex.ru>
2026-03-20 00:32:24 +0700

    style(ui): изменяет отступы секции контактов

--- app/components/sections/ContactInfo.vue
+++ app/components/sections/ContactInfo.vue
@@ -48,7 +48,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <section id="contacts" class="w-full bg-white pt-60 pb-15">
+    <section id="contacts" class="w-full bg-white pt-10 pb-15 md:pt-18">
         <div class="mx-auto max-w-292.5 px-4 md:px-6 lg:px-8">
             <!-- Заголовок секции -->
             <h2 class="font-montserrat mb-8 text-center text-[32px] font-semibold text-[#2C2C2C] md:text-[42px]">
